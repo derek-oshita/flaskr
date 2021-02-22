4affbced96db94adcc94a133827c94a1c795f06a
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-02-21 18:48:25 -0800

    For loop.

--- flaskblog.py
+++ flaskblog.py
@@ -4,11 +4,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Derek Oshita", 
+        "title": "Freakin blog dude", 
+        "content": "First content", 
+        "date_posted": "April 14, 1992"
+
+    }, 
+        {
+        "author": "Mila Oshita", 
+        "title": "Best lady", 
+        "content": "Second content", 
+        "date_posted": "April 20, 1992"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home(): 
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about(): 
