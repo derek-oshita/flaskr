2611a4baa5323ff37ff003e897245fcbd4952def
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-02-21 20:16:04 -0800

    Fixed the forms with bootstrap classes.

--- flaskblog.py
+++ flaskblog.py
@@ -5,7 +5,7 @@ from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-app.config['SECRET KEY'] = 'c2d449cb7fec0e2c5efca449a6dcc6a7'
+app.config['SECRET_KEY'] = 'c2d449cb7fec0e2c5efca449a6dcc6a7'
 
 posts = [
     {
