572b7bfcd14c99c3b0e7bef4c60499a14e4a58ee
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-02-21 18:41:34 -0800

    Basic route and view set up.

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,18 @@
 import os
 
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world(): 
-    return "<h1>Home page!</h1>"
+@app.route("/home")
+def home(): 
+    return render_template("home.html")
 
 @app.route("/about")
 def about(): 
-    return "About page..."
+    return render_template("about.html")
 
 if __name__ == "__main__": 
     app.run(debug=True, port=3000)
