53f219029001e58e855dee7e91be9c9d646aa01a
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-02-22 18:59:13 -0800

    Delete post route and delete modal working.

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -118,7 +118,6 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post)
 
 # UPDATE POST
-
 @app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
@@ -137,3 +136,15 @@ def update_post(post_id):
         form.content.data = post.content
 
     return render_template('create_post.html', title='Update Post', form=form, legend='Update Post') 
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)    
+    if post.author != current_user: 
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f"Your post has been deleted!", 'success')
+    return redirect(url_for('home'))
