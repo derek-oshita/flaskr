9728444f1f907c20035afc201354edeb4acba553
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-02-22 18:33:50 -0800

    Update post route/form working.

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -109,7 +109,7 @@ def new_post():
         db.session.commit()
         flash(F"Post created!", 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', legend='New Post', form=form)
 
 # GET POST
 @app.route('/post/<int:post_id>')
@@ -118,11 +118,22 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post)
 
 # UPDATE POST
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
 @login_required
-@app.route('/post/<int:post_id>/update')
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)    
     if post.author != current_user: 
         abort(403)
     form = PostForm()
-    return render_template('create_post.html', title='Update Post') 
+    if form.validate_on_submit(): 
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash(f"Your post has been updated!", 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET': 
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post') 
